738c8e398b30a904e7f25d9b0610dda7c07c044e
HamidReza Maneshti <46029813+hamidrezamaneshti@users.noreply.github.com>
2023-05-03 15:08:14 +0330

    Update SendDataToCloud.py

--- SendDataToCloud.py
+++ SendDataToCloud.py
@@ -3,56 +3,51 @@ import time
 from datetime import datetime
 import csv
 
-#config database
+# Configure the database
 config = {
-  "apiKey": "AIzaSyBmSb9Rk1JQD-kGzpYWx-xxh_SSbvQOGgg",
-  "authDomain": "ecgfirebaseproject-673ed.firebaseapp.com",
-  "databaseURL": "https://ecgfirebaseproject-673ed.firebaseio.com/",
-  "storageBucket": "ecgfirebaseproject-673ed.appspot.com"
-  }
-fb=pyrebase.initialize_app(config)
+    "apiKey": "******",
+    "authDomain": "******",
+    "databaseURL": "https://ecgfirebaseproject-673ed.firebaseio.com/",
+    "storageBucket": "ecgfirebaseproject-673ed.appspot.com"
+}
+fb = pyrebase.initialize_app(config)
 
 # Get a reference to the auth service
 auth = fb.auth()
 db = fb.database()
-#authenticate a user
-user = auth.sign_in_with_email_and_password("hamidreza.maneshti@gmail.com", "hamidreza3703100")
 
+# Authenticate a user
+user = auth.sign_in_with_email_and_password("****", "****")
 
-count=0
+count = 0
 timest1 = datetime.now().strftime("%H:%M:%S")
 timeday = time.strftime('%d-%m-%Y')
-#loop
-i=0
-a=0
-while count<10:
+
+# Loop until count reaches 10
+i = 0
+a = 0
+while count < 10:
     timest2 = datetime.now().strftime("%H:%M:%S")
 
-    #time_stamp
-    c=""
-    # timest = datetime.now().strftime("%H:%M:%S:%f")
-    print("time before:",timest2)
-    timest=datetime.now().timestamp()
+    # Time stamp
+    c = ""
+    print("time before:", timest2)
+    timest = datetime.now().timestamp()
 
     bi = 1
-    with open('healthcare/Sample_Dataset','r')as csvFile:
-        reader=csv.reader(csvFile, None)
-        #assert isinstance(reader)
+    with open('healthcare/Sample_Dataset', 'r') as csvFile:
+        reader = csv.reader(csvFile, None)
 
         for row in reader:
-            r=row[0]
-            c=str(r)
+            r = row[0]
+            c = str(r)
             print("r in loop: ", c, "  x in loop: ", a)
-    #add data to firebase
-            ecg={"xValue" :a,"yValue":c}
-            a+=1
-    #ecg = {"xValue": xvalue, "yValue": yvalue}
-
+            
+            # Add data to Firebase
+            ecg = {"xValue": a, "yValue": c}
+            a += 1
             db.child("ecgdata").push(ecg, user['idToken'])
 
-    #db.child("chartTable").push(ecg, user['idToken'])
-    #db.child("ECG "+ timeday+"--"+timest1).push(ecg, user['idToken'])
-    #db.child("temp " + timeday + "--" + timest1).update({timest: "hamid"}, user['idToken'])
-    count+=1
+    count += 1
 
-print("time aftre while:",timest2)
+print("time after while:", timest2)
